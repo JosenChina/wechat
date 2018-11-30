cc643668bfa0e9b8249be8133cf9b7a3c70a3ab8
肉松 <JosenChina@126.com>
2018-11-30 16:17:17 +0800

    delete the test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import json
-wx_ticket_dict = eval("{'ret': '0', 'message': '', 'skey': '@crypt_763802bb_0a8c4204095e2aa51c2f15d48a1abee7', 'wxsid': 'nLdJTV8K+JOaPjB+', 'wxuin': '3449552453', 'pass_ticket': 'fbpFrM21i%2BIt3dylyc53QfpMqoO0s%2B4XoCeaQIEj51Rc%2BZM%2FczEhXSNJyotQFB54', 'isgrayscale': '1'}")
-
-for k, v in wx_ticket_dict.items():
-    print(k, v)
-
-
